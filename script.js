96fefb2e25af72e2948c4d549b52219b6a7e48b5
AbdElrahman <145148937+abdElrahman200@users.noreply.github.com>
2023-10-18 17:56:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let avt = document.getElementsByClassName('avt');
+for(let x=0 ;x<avt.length;x++){
+    avt[x].style.zIndex = avt.length - x;
+}
+
+
+let j =1;
+
+function lami(i){
+
+if(i.value == 0 || i==''){
+    avt[0].style.opacity = 0
+}else{
+    avt[j].style.opacity =0;
+     if(j==5){
+        for(let u =0 ;u<avt.length ;u++){
+            avt[u].style.opacity =1;
+            j=0
+        }
+     }else{j++}
+if(i=='r'){
+    for(let u =0 ;u<avt.length ;u++){
+        avt[u].style.opacity =1;
+        
+    }
+
+
+}
+
+if(i=='p'){
+    for(let u =0 ;u<avt.length ;u++){
+        avt[u].style.opacity =0;
+        
+    }
+
+    avt[avt.length -1].style.opacity =1
+
+}
+    }
+
+}
